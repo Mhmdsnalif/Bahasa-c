feb821b1cab9454fda3b6d50a161da24648a604d
Mhmdsnalif <55049271+Mhmdsnalif@users.noreply.github.com>
2021-02-15 19:50:10 +0700

    Create menghitung_phi.c

---
+++ latihan_c/menghitung_phi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PHI 3.14 //deklarasi konstanta
+
+int main(){
+
+    float jari, luas; //deklarasai tipe data
+
+    printf("=== Menghitung Luas Lingkaran ===\n\n");
+
+    printf("Jari - Jari Lingkaran = 2\n");
+    luas = PHI*2*2;
+
+    printf("Luas Lingkarang adalah %.2f\n\n",luas);
+return 0;
+}
